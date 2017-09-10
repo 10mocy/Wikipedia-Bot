b873ec4656b45eea2f4e4c0118f378594cff02a0
小野咲みなみ <mnmonzk@gmail.com>
2017-09-10 22:36:47 +0900

    [追加]メッセージ受信処理を実装

--- wikipedia.js
+++ wikipedia.js
@@ -4,3 +4,22 @@ const token = new token();
 const client = new Discord.Client();
 
 client.login(token);
+
+client.on("ready", () => {
+    writeLog("システム", "準備が整いました。");
+});
+
+client.on("message", message => {
+    const sender = message.member;
+    if(sender.id !== "356430678684073986") {
+        writeLog("メッセージ受信", message.content + " <--- " + sender.user.tag + "(#" + message.channel.name + " << " + message.guild.name + ")");
+    }
+});
+function sendMessage(channel, message) {
+    writeLog("メッセージ送信", message + " ---> #" + channel.name);
+    channel.send(message);
+}
+
+function writeLog(title, detail) {
+    console.log("-----\n"+ title + "\n\n    " + detail + "\n");
+}
